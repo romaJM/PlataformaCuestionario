6c181f4ea60b2bb2a24ee2dabefad6ee312e33d2
romaJM <romane.jacha@gmail.com>
2025-12-02 20:00:55 -0400

    para que cuando se registre un usuario el password no sea visible

--- Models/usuario.js
+++ Models/usuario.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/conection');
 const Rol = require('./rol');
+const bcrypt = require('bcryptjs');
 
 const Usuario = sequelize.define('Usuario', {
   id_usuario: {
@@ -34,9 +35,24 @@ const Usuario = sequelize.define('Usuario', {
   }
 }, {
   tableName: 'usuarios',
+  timestamps: false,
   
-});
+  hooks: {
+    //  Se ejecuta antes de crear el usuario
+    beforeCreate: async (usuario) => {
+      if (usuario.password) {
+        usuario.password = await bcrypt.hash(usuario.password, 10);
+      }
+    },
 
+    //  por si elusuario cambia la contraseña
+    /*beforeUpdate: async (usuario) => {
+      if (usuario.changed('password')) {
+        usuario.password = await bcrypt.hash(usuario.password, 10);
+      }
+    }*/
+  }
+});
 
 Usuario.belongsTo(Rol, {
   foreignKey: 'id_rol'
